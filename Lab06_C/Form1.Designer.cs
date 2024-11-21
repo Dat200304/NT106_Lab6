83b3533576f3a5d7faa4b7b1e30052eaa6bdb76a
Dat <tandat41202gmail.com>
2024-11-21 13:40:21 +0700

    Lab6 NT106

---
+++ Lab06_C/Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace Lab06_C
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            txtName = new TextBox();
+            txtGuess = new TextBox();
+            btnSubmit = new Button();
+            lblMessage = new Label();
+            lblRange = new Label();
+            timer = new System.Windows.Forms.Timer(components);
+            txtRoundsLeft = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            btnAutoPlay = new Button();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            SuspendLayout();
+            // 
+            // txtName
+            // 
+            txtName.Location = new Point(154, 144);
+            txtName.Multiline = true;
+            txtName.Name = "txtName";
+            txtName.Size = new Size(270, 43);
+            txtName.TabIndex = 0;
+            // 
+            // txtGuess
+            // 
+            txtGuess.Location = new Point(180, 278);
+            txtGuess.Multiline = true;
+            txtGuess.Name = "txtGuess";
+            txtGuess.Size = new Size(124, 45);
+            txtGuess.TabIndex = 1;
+            // 
+            // btnSubmit
+            // 
+            btnSubmit.BackColor = Color.PapayaWhip;
+            btnSubmit.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnSubmit.Location = new Point(311, 265);
+            btnSubmit.Name = "btnSubmit";
+            btnSubmit.Size = new Size(113, 65);
+            btnSubmit.TabIndex = 2;
+            btnSubmit.Text = "Đoán";
+            btnSubmit.UseVisualStyleBackColor = false;
+            btnSubmit.Click += btnSubmit_Click_1;
+            // 
+            // lblMessage
+            // 
+            lblMessage.BackColor = Color.White;
+            lblMessage.ForeColor = SystemColors.ActiveCaptionText;
+            lblMessage.Location = new Point(12, 9);
+            lblMessage.Name = "lblMessage";
+            lblMessage.Size = new Size(412, 120);
+            lblMessage.TabIndex = 3;
+            lblMessage.Text = "Thông báo";
+            // 
+            // lblRange
+            // 
+            lblRange.BackColor = Color.White;
+            lblRange.ForeColor = SystemColors.ControlText;
+            lblRange.Location = new Point(190, 201);
+            lblRange.Name = "lblRange";
+            lblRange.Size = new Size(234, 50);
+            lblRange.TabIndex = 4;
+            // 
+            // txtRoundsLeft
+            // 
+            txtRoundsLeft.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            txtRoundsLeft.Location = new Point(634, 12);
+            txtRoundsLeft.Multiline = true;
+            txtRoundsLeft.Name = "txtRoundsLeft";
+            txtRoundsLeft.Size = new Size(82, 71);
+            txtRoundsLeft.TabIndex = 6;
+            txtRoundsLeft.TextAlign = HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            label1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(9, 152);
+            label1.Name = "label1";
+            label1.Size = new Size(143, 53);
+            label1.TabIndex = 7;
+            label1.Text = "Tên người chơi";
+            // 
+            // label2
+            // 
+            label2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Location = new Point(48, 283);
+            label2.Name = "label2";
+            label2.Size = new Size(100, 29);
+            label2.TabIndex = 8;
+            label2.Text = "Số đoán";
+            // 
+            // label3
+            // 
+            label3.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.Location = new Point(445, 29);
+            label3.Name = "label3";
+            label3.Size = new Size(190, 29);
+            label3.TabIndex = 9;
+            label3.Text = "Số vòng chơi còn lại";
+            // 
+            // btnAutoPlay
+            // 
+            btnAutoPlay.BackColor = Color.PapayaWhip;
+            btnAutoPlay.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnAutoPlay.Location = new Point(463, 240);
+            btnAutoPlay.Name = "btnAutoPlay";
+            btnAutoPlay.Size = new Size(136, 61);
+            btnAutoPlay.TabIndex = 10;
+            btnAutoPlay.Text = "AutoPlay";
+            btnAutoPlay.UseVisualStyleBackColor = false;
+            btnAutoPlay.Click += btnAutoPlay_Click_1;
+            // 
+            // label4
+            // 
+            label4.BackColor = Color.White;
+            label4.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label4.Location = new Point(617, 228);
+            label4.Name = "label4";
+            label4.Size = new Size(91, 84);
+            label4.TabIndex = 11;
+            label4.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            label5.BackColor = Color.LightSeaGreen;
+            label5.Location = new Point(445, 201);
+            label5.Name = "label5";
+            label5.Size = new Size(271, 131);
+            label5.TabIndex = 12;
+            // 
+            // label6
+            // 
+            label6.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label6.Location = new Point(48, 222);
+            label6.Name = "label6";
+            label6.Size = new Size(90, 29);
+            label6.TabIndex = 13;
+            label6.Text = "Phạm vi";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Aquamarine;
+            ClientSize = new Size(720, 361);
+            Controls.Add(label6);
+            Controls.Add(label4);
+            Controls.Add(btnAutoPlay);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(txtRoundsLeft);
+            Controls.Add(lblRange);
+            Controls.Add(lblMessage);
+            Controls.Add(btnSubmit);
+            Controls.Add(txtGuess);
+            Controls.Add(txtName);
+            Controls.Add(label5);
+            Name = "Form1";
+            Text = "Client";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+
+        private System.Windows.Forms.Timer timer;
+      
+
+        // Trong Form1.Designer.cs
+        private System.Windows.Forms.TextBox txtRoundsLeft;
+        private System.Windows.Forms.Label lblRange;
+        private System.Windows.Forms.Label lblMessage;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtGuess;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Button btnAutoPlay;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+
+        // Đảm bảo các điều khiển được khởi tạo đúng trong InitializeComponent()
+    }
+}
---
+++ Lab06_C/Form1.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace Lab06_C
+{
+    public partial class Form1 : Form
+    {
+        //Danh sách các biến 
+        private TcpClient client;
+        private StreamReader reader;
+        private StreamWriter writer;
+        private int minRange;
+        private int maxRange;
+        private System.Windows.Forms.Timer autoPlayTimer;
+        private Random random = new Random();
+        private HashSet<int> guessedNumbers = new HashSet<int>();
+        private bool isAutoPlaying = false;
+
+        // Thêm các biến để lưu trữ thông tin người chơi
+        private int correctGuesses = 0;
+        private int incorrectGuesses = 0;
+        private int score = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            await ConnectToServer();
+        }
+
+
+        //Kết nối tới Server 
+        private async Task ConnectToServer()
+        {
+            client = new TcpClient();
+            await client.ConnectAsync("127.0.0.1", 8888);
+            var stream = client.GetStream();
+            reader = new StreamReader(stream);
+            writer = new StreamWriter(stream);
+            writer.AutoFlush = true;
+
+            Task.Run(() => ListenToServer());
+        }
+
+
+        //Lắng nghe từ Server để nhận phạm vi đoán số, số vòng chơi ,...
+        private async Task ListenToServer()
+        {
+            while (true)
+            {
+                var line = await reader.ReadLineAsync();
+                if (line != null)
+                {
+                    var message = JsonConvert.DeserializeObject<ServerMessage>(line);
+                    ProcessServerMessage(message);
+                }
+            }
+        }
+
+        //Xử lí thông điệp được gửi từ Server
+        private void ProcessServerMessage(ServerMessage message)
+        {
+            switch (message.Type)
+            {
+                case "range":
+                case "new_round":                            //Vòng mới 
+                    minRange = message.MinRange;
+                    maxRange = message.MaxRange;
+                    guessedNumbers.Clear();
+                    Invoke((MethodInvoker)delegate
+                    {
+                        lblRange.Text = $"{minRange} - {maxRange}";
+                        lblMessage.Text = "Vòng mới đã bắt đầu! ";
+                        txtRoundsLeft.Text = message.RoundsLeft.ToString();
+                        txtGuess.Text = "";
+                    });
+                    break;
+                case "winner":                               //Người chiến thắng nếu đoán đúng
+                    var winner = message.Winner;
+                    if (winner == txtName.Text)
+                    {
+                        correctGuesses++;
+                        score += 10;
+                    }
+                    Invoke((MethodInvoker)delegate
+                    {
+                        lblMessage.Text = $"{winner} đã chiến thắng. Điểm của bạn : {score}";
+                        txtRoundsLeft.Text = message.RoundsLeft.ToString();
+                        autoPlayTimer?.Stop();
+                    });
+                    break;
+                case "incorrect_guess":                       //Đoán sai 
+                    incorrectGuesses++;
+                    score = message.Score; // Cập nhật điểm từ Server 
+                    Invoke((MethodInvoker)delegate
+                    {
+                        lblMessage.Text = $"Đoán sai : {message.Guess}. Điểm của bạn : {score}";
+                        txtGuess.Text = "";
+                    });
+                    break;
+                case "end_game":                              //Kết thúc game 
+                    // Hiện tất cả thông báo cuối cùng khi kết thúc game 
+                    Invoke((MethodInvoker)delegate
+                    {
+                        lblMessage.Text = $"Game over! Người thắng : {message.Winner}\nSố câu đúng của bạn : {correctGuesses}\nSố câu sai của bạn : {incorrectGuesses}\nĐiểm của bạn : {score}";
+                    });
+                    break;
+            }
+        }
+
+        //Nút gửi Số Đoán bằng tay 
+        private async void btnSubmit_Click_1(object sender, EventArgs e)
+        {
+            var guess = new Guess
+            {
+                Name = txtName.Text,
+                Number = int.Parse(txtGuess.Text)
+            };
+            var guessJson = JsonConvert.SerializeObject(guess);
+            await writer.WriteLineAsync(guessJson);
+        }
+
+
+        //Hàm tự động đoán số 
+        private async void MakeAutoGuess()
+        {
+            int autoGuess;
+            do
+            {
+                autoGuess = random.Next(minRange, maxRange + 1);
+            } while (guessedNumbers.Contains(autoGuess));
+
+            guessedNumbers.Add(autoGuess);
+
+            Invoke((MethodInvoker)delegate
+            {
+                label4.Text = autoGuess.ToString(); // Hiển thị số ngẫu nhiên lên label4
+            });
+
+            var guess = new Guess
+            {
+                Name = txtName.Text,
+                Number = autoGuess
+            };
+            var guessJson = JsonConvert.SerializeObject(guess);
+            await writer.WriteLineAsync(guessJson);
+        }
+
+
+        //Nút kích hoạt chế độ Tự động đoán số 
+        private void btnAutoPlay_Click_1(object sender, EventArgs e)
+        {
+            isAutoPlaying = !isAutoPlaying;
+            if (isAutoPlaying)
+            {
+                lblMessage.Text = "Auto play is ON";
+                StartAutoPlay();
+            }
+            else
+            {
+                lblMessage.Text = "Auto play is OFF";
+                autoPlayTimer?.Stop();
+            }
+        }
+        
+
+        //Hàm bắt đầu tự động đoán 
+        private void StartAutoPlay()
+        {
+            autoPlayTimer = new System.Windows.Forms.Timer();
+            autoPlayTimer.Interval = 3000; // Mỗi lần dự đoán cách 3 giây 
+            autoPlayTimer.Tick += (s, e) => MakeAutoGuess();
+            autoPlayTimer.Start();
+            MakeAutoGuess(); 
+        }
+
+        public class ServerMessage
+        {
+            public string Type { get; set; }
+            public int MinRange { get; set; }
+            public int MaxRange { get; set; }
+            public string Winner { get; set; }
+            public int SecretNumber { get; set; }
+            public int RoundsLeft { get; set; }
+            public int Guess { get; set; }
+            public int Score { get; set; }
+            public int CorrectGuesses { get; set; }
+            public int IncorrectGuesses { get; set; }
+        }
+
+        public class Guess
+        {
+            public string Name { get; set; }
+            public int Number { get; set; }
+        }
+    }
+}
